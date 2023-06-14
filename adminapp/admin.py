c8ebd9e03ba8a3acf12ada22859d6aa2223d428c
anuabhi431 <anurveshcivil5@gmail.com>
2023-06-14 15:09:58 +0530

    Initial commit

---
+++ adminapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Jobs
+
+# Register your models here.
+class JobsModelAdmin(admin.ModelAdmin):
+    list_display=['jobid','title','skills','description']
+    list_filter = ['title']
+    class Meta:
+        model=Jobs
+admin.site.register(Jobs,JobsModelAdmin)
